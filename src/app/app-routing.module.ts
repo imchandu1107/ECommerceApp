2f658b44c5aeb905638855c2bf632ac22fc4b702
Chandra Kiran G <chandug1107@gmail.com>
2021-06-12 01:00:51 +0530

    components created and created db.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { LaptopsComponent } from './laptops/laptops.component';
+import { LoginComponent } from './login/login.component';
+import { MobilesComponent } from './mobiles/mobiles.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+import { TelevisionsComponent } from './televisions/televisions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'contactus',component:ContactusComponent},
+  {path:'products',component:ProductsComponent,
+  children:[
+    {path:'mobiles',component:MobilesComponent},
+    {path:'televisions',component:TelevisionsComponent},
+    {path:'laptops',component:LaptopsComponent},
+    {path:'',redirectTo:'/products/mobiles',pathMatch:'full'}
+  ]},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'**',component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LaptopsComponent } from './laptops/laptops.component';
+import { LoginComponent } from './login/login.component';
+import { MobilesComponent } from './mobiles/mobiles.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ProdDetailsComponent } from './prod-details/prod-details.component';
+import { ProductsComponent } from './products/products.component';
+import { RegisterComponent } from './register/register.component';
+import { TelevisionsComponent } from './televisions/televisions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactusComponent,
+    FooterComponent,
+    HomeComponent,
+    LaptopsComponent,
+    LoginComponent,
+    MobilesComponent,
+    PagenotfoundComponent,
+    ProdDetailsComponent,
+    ProductsComponent,
+    RegisterComponent,
+    TelevisionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
