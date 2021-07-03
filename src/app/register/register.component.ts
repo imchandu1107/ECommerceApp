ef9e2a7ef2f47faaf23b75f14e3191bf979ca243
Chandra Kiran G <chandug1107@gmail.com>
2021-07-03 12:46:25 +0530

    user registration implemented

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,13 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor( private us: UserService, private router: Router ) { }
 
   ngOnInit(): void {
   }
 
   onSignup( userObj:any ) {
-    console.log( userObj )
+    this.us.createUser( userObj ).subscribe(
+      res => {
+        if ( res.message === "New user created successfully" ) {
+          alert( "New user created successfully" )
+          //navigate to login component
+          this.router.navigateByUrl( "/login" )
+        }
+        else {
+          alert( res.message )
+        }
+      },
+      err => {
+        console.log( err )
+        alert("Something went wrong in user registration try again later")
+      }
+    )
   }
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor( private hc: HttpClient ) { }
+  createUser( userObj: any ): Observable<any>{
+     return this.hc.post( "/user/createuser", userObj )
+  }
 }
